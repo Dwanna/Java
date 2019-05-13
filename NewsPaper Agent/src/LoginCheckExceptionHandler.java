1de7932044e4b4a5423ea55359886ec4bf1e62ca
Dwanna <Dwanna@github.com>
2019-05-14 00:51:57 +0100

    Newspaper Agent

---
+++ NewsPaper Agent/src/LoginCheckExceptionHandler.java
@@ -0,0 +1,11 @@
+public class LoginCheckExceptionHandler extends Exception {
+	String errorMessage;
+
+	public LoginCheckExceptionHandler(String errMsg) {
+		errorMessage = errMsg;
+	}
+
+	public String getMessage() {
+		return errorMessage;
+	}
+}
---
+++ NewsPaper Agent/src/Manager.java
@@ -0,0 +1,17 @@
+public class Manager {
+	boolean validateLogin(String username, String password) {
+		boolean res = false;
+		try {
+			if (username.equals("Admin") && password.equals("Pass")) {
+				res = true;
+
+			} else {
+				throw new LoginCheckExceptionHandler("Invalid Login");
+			}
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return res;
+	}
+
+}
